d0a9fc52567ecfe5885469f73ef364ab12865914
hyper983 <amirhosseinmirzaei@yandex.com>
2026-01-05 23:22:44 +0330

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Advanced Bodycam Script'
+version '1.0.0'
+author 'HyperV'
+ui_page 'ui.html'
+
+files {
+    'ui.html'
+}
+
+client_script 'client.lua'
+server_script 'server.lua'
